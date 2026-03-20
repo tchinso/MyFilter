6b0ecf227c8ba65b17883ce251ff3d32b4eb8188
냥캣 <132228523+tchinso@users.noreply.github.com>
2026-03-21 02:36:07 +0900

    Update url.js

--- url.js
+++ url.js
@@ -3,8 +3,9 @@
   // 새 규칙 추가 예시:
   // { keyword: "blog", target: "https://example.com/blog" },
   const redirectRules = [
-    { keyword: "video", target: "https://www.youtube.com/watch?v=DBcP4hq7oUU" },
     { keyword: "twt", target: "https://x.com/i/communities/1489301791259115521" },
+    { keyword: "video", target: "https://www.youtube.com/watch?v=DBcP4hq7oUU" },
+    { keyword: "base64", target: "https://tchinso.github.io/Favorites/app/TextDecoder/" },
   ];
 
   const currentUrl = decodeURIComponent(window.location.href).toLowerCase();
